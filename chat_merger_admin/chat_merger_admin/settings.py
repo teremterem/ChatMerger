595f412e39b0f7607c72fd86ace32d5f64f6fa4b
Oleksandr Tereshchenko <toporok@gmail.com>
2023-05-07 12:32:23 +0300

    ChatMerger

--- chat_merger_admin/chat_merger_admin/settings.py
+++ chat_merger_admin/chat_merger_admin/settings.py
@@ -1,3 +1,4 @@
+# pylint: disable=wrong-import-position,import-error
 """
 Django settings for chat_merger_admin project.
 
@@ -9,14 +10,14 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import sys
 from pathlib import Path
-
-from chat_merger_config import DJANGO_SECRET_KEY, DJANGO_DEBUG
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(BASE_DIR.parent))  # add ChatMerger project root to path
 
+from chat_merger_config import DJANGO_SECRET_KEY, DJANGO_DEBUG
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -28,7 +29,6 @@ SECRET_KEY = DJANGO_SECRET_KEY
 DEBUG = DJANGO_DEBUG
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "chat_merger_admin.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -81,7 +80,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -101,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -112,7 +109,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
